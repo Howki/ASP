7e18a847d4957335b94d3539fc27f30d0e82b0ea
Howki <howki8000@gmail.com>
2018-02-04 00:04:51 +0300

    2lab 80%

---
+++ 2lab/2lab.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cacheSize ()
+{
+  invokeToDec $1 $2 $3
+}
+
+invokeToDec ()
+{
+  echo 123
+}
+
+SIZE_MATRIX=4096 
+
+echo "ASP - 2 lab. work."
+
+NOS_CACHE1=`cpuid | grep -A14 "cache 0" | grep "number of sets" | cut -d= -f2 | head -1 | cut -d" " -f2`
+WAYS_CACHE1=`cpuid | grep -A14 "cache 0" | grep "ways of associativity" | cut -d= -f2 | head -1 | cut -d" " -f2
+LINE_SIZE_CACHE1=`cpuid | grep -A14 "cache 0" | grep "system coherency line size" | cut -d= -f2 | head -1 | cut -d" " -f2
+
+NOS_CACHE2=`cpuid | grep -A14 "cache 2" | grep "number of sets" | cut -d= -f2 | head -1 | cut -d" " -f2`
+WAYS_CACHE2=`cpuid | grep -A14 "cache 2" | grep "ways of associativity" | cut -d= -f2 | head -1 | cut -d" " -f2`
+LINE_SIZE_CACHE2=`cpuid | grep -A14 "cache 2" | grep "system coherency line size" | cut -d= -f2 | head -1 | cut -d" " -f2`
+
+NOS_CACHE3=`cpuid | grep -A14 "cache 3" | grep "number of sets" | cut -d= -f2 | head -1 | cut -d" " -f2`
+WAYS_CACHE3=`cpuid | grep -A14 "cache 3" | grep "ways of associativity" | cut -d= -f2 | head -1 | cut -d" " -f2`
+LINE_SIZE_CACHE3=`cpuid | grep -A14 "cache 3" | grep "system coherency line size" | cut -d= -f2 | head -1 | cut -d" " -f2`
+
+let "SIZE_CACHE0 = ($NOS_CACHE1 + 1) * 8 * 64"
+let "SIZE_CACHE1 = ($NOS_CACHE2 + 1) * 8 * 64"
+let "SIZE_CACHE2 = ($NOS_CACHE3 + 1) * 16 * 64"
+
+./bin/2lab $SIZE_CACHE0 $SIZE_CACHE1 $SIZE_CACHE2 $SIZE_MATRIX
--- 2lab/src/2lab.c
+++ 2lab/src/2lab.c
@@ -6,57 +6,94 @@
 
 static __inline__ unsigned long long rdtsc(void);
 
-void mullOptMatrix(float *, float *, float *, int);
-void mullBlocks(float *, float *, float *, int);
+void mullOptMatrix(float *, float *, float *, int, int);
+void mullBlocks(float *, float *, float *, int, int, int, int);
 
-void mullMatrix(float *, float *, float *, int); // +
+void mullMatrix(float *, float *, float *, int);
 
-void fillMatrixRandElem(float *, int); // +
+void fillMatrixRandElem(float *, int);
+void fillMatrixZeroElem(float *, int);
 
-int compareMatrix(float *, float *, int); // +
+int compareMatrix(float *, float *, int);
 
-float* allocMemMatrix(int); // +
-void freeMemMatrix(float *); // +
+float* allocMemMatrix(int);
+void freeMemMatrix(float *);
 
-void printMatrix(float *, int); // +
+void printMatrix(float *, int);
 
-const int MAX_RAND_DIV = 128;
+const int MAX_RAND_DIV = 4;
 
 int main(int argc, char **argv) {
 
-  int sizeMatrix;
-  int sizeBlockL1;
-  int sizeBlockL2;
-  int sizeBlockL3;
-
-  if(argc == 4) {
-    sizeMatrix = sizeBlockL3 * 4;
-    sizeBlockL1 = sizeBlockL1 * 0.8 / 3;
-    sizeBlockL2 = sizeBlockL2 * 0.8 / 3;
-    sizeBlockL3 = sizeBlockL3 * 0.8 / 3;
-  } else {
+  if(argc != 5) {
     printf("Incorrect arguments!\n");
     return 1;
   }
 
-  float *matrixA = allocMemMatrix(5);
-  float *matrixB = allocMemMatrix(5);
-  float *matrixC = allocMemMatrix(5);
-  float *matrixD = allocMemMatrix(5);
+  int sizeMatrix = atoi(argv[4]);
+  int sizeCacheL1 = atoi(argv[1]);
+  int sizeCacheL2 = atoi(argv[2]);
+  int sizeCacheL3 = atoi(argv[3]);
 
-  fillMatrixRandElem(matrixA, 5);
-  fillMatrixRandElem(matrixB, 5);
+  printf("\nL1 size - %d (bytes)\n", sizeCacheL1);
+  printf("L2 size - %d (bytes)\n", sizeCacheL2);
+  printf("L3 size - %d (bytes)\n\n", sizeCacheL3);
 
+  int sizeBlockL1 = (int)(sqrt(sizeCacheL1) * 0.8 / 3) & 0xFFFFFFF0;
+  int sizeBlockL2 = (int)(sqrt(sizeCacheL2) * 0.8 / 3) & 0xFFFFFFF0;
+  int sizeBlockL3 = (int)(sqrt(sizeCacheL3) * 0.8 / 3) & 0xFFFFFFF0;
+
+  printf("L1 cache block size: %d\n", sizeBlockL1);
+  printf("L2 cache block size: %d\n", sizeBlockL2);
+  printf("L3 cache block size: %d\n", sizeBlockL3);
+
+  float *matrixA = allocMemMatrix(sizeMatrix);
+  float *matrixB = allocMemMatrix(sizeMatrix);
+  float *matrixC = allocMemMatrix(sizeMatrix);
+  float *matrixD = allocMemMatrix(sizeMatrix);
+
+  fillMatrixRandElem(matrixA, sizeMatrix);
+  fillMatrixRandElem(matrixB, sizeMatrix);
+
+  /*L3*/
+  printf("\nL3:\n");
   unsigned long long timeElapsed = rdtsc();
 
-  mullMatrix(matrixA, matrixB, matrixD, 5);
+  mullOptMatrix(matrixA, matrixB, matrixC, sizeMatrix, sizeBlockL3);
   
-  printf("Elapsed time (opt): %.5f (seconds).\n", (rdtsc() - timeElapsed) / pow(2, 31));
+  printf("Elapsed time (L3): %.5f (seconds).\n", (rdtsc() - timeElapsed) / pow(2, 31));
+  
+  /*L2*/
+  printf("\nL2:\n");
+  fillMatrixZeroElem(matrixC, sizeMatrix);
+  timeElapsed = rdtsc();
 
-  /*if(compareMatrix(matrixC, matrixD))*/
-    /*printf("Equal.\n");*/
-  /*else*/
-    /*printf("Not Equal.\n");*/
+  mullOptMatrix(matrixA, matrixB, matrixC, sizeMatrix, sizeBlockL2);
+  
+  printf("Elapsed time (L2): %.5f (seconds).\n", (rdtsc() - timeElapsed) / pow(2, 31));
+
+  
+  /*L1*/
+  printf("\nL1:\n");
+  fillMatrixZeroElem(matrixC, sizeMatrix);
+  timeElapsed = rdtsc();
+
+  mullOptMatrix(matrixA, matrixB, matrixC, sizeMatrix, sizeBlockL1);
+  
+  printf("Elapsed time (L1): %.5f (seconds).\n", (rdtsc() - timeElapsed) / pow(2, 31));
+
+  /*Without cache opt*/
+  printf("\nWithout cache opt:\n");
+  timeElapsed = rdtsc();
+
+  mullMatrix(matrixA, matrixB, matrixD, sizeMatrix);
+
+  printf("Elapsed time (no opt): %.5f (seconds).\n", (rdtsc() - timeElapsed) / pow(2, 31));
+
+  if(compareMatrix(matrixC, matrixD, sizeMatrix))
+    printf("Equal.\n");
+  else
+    printf("Not Equal.\n");
 
   freeMemMatrix(matrixA);
   freeMemMatrix(matrixB);
@@ -67,42 +104,56 @@ int main(int argc, char **argv) {
 }
 
 void printMatrix(float *matrix, int sizeMatrix) {
-  for(int i = 0; i < sizeMatrix; i++)
+  for(int i = 0; i < sizeMatrix; i++) {
     for(int j = 0; j < sizeMatrix; j++) {
       printf("%.1f ", matrix[i * sizeMatrix + j]);
-      printf("\n");
     }
+    printf("\n");
+  }
   printf("\n");
 }
 
 int compareMatrix(float *matrixA, float *matrixB, int sizeMatrix) {
   for(int i = 0; i < sizeMatrix; i++)
     for(int j = 0; j < sizeMatrix; j++) {
-      if(matrixA[i * sizeMatrix + j] != matrixB[i * sizeMatrix + j]);
-      return 0;
+      if(matrixA[i * sizeMatrix + j] != matrixB[i * sizeMatrix + j])
+        return 0;
     }
 
   return 1;
 }
 
-void mullOptMatrix(float *matrixA, float *matrixB, float *matrixC, int sizeMatrix) {
-  for(int i = 0; i < sizeMatrix; i++)
-    for(int j = 0; j < sizeMatrix; j++)
-      for(int k = 0; k < sizeMatrix; k++)
-        mullBlocks(&matrixA[i], matrixB, matrixC, 5);
+void mullOptMatrix(float *matrixA, float *matrixB, float *matrixC, int sizeMatrix, int sizeBlock) {
+  int numberOfBlocks = ceil((float)sizeMatrix / sizeBlock);
+
+  printf("Number of blocks: %d\n", numberOfBlocks);
+
+  for(int i = 0, sizeIBlock = sizeBlock; i < numberOfBlocks; i++) {
+    if(i == numberOfBlocks - 1)
+      sizeIBlock = sizeMatrix - sizeBlock * i;
+    for(int j = 0, sizeJBlock = sizeBlock; j < numberOfBlocks; j++) {
+      if(j == numberOfBlocks - 1)
+        sizeJBlock = sizeMatrix - sizeBlock * j;
+      for(int k = 0, sizeKBlock = sizeBlock; k < numberOfBlocks; k++) {
+        if(k == numberOfBlocks - 1)
+          sizeKBlock = sizeMatrix - sizeBlock * k;
+        mullBlocks(&matrixA[i * sizeMatrix * sizeBlock + k * sizeBlock], &matrixB[k * sizeMatrix * sizeBlock + j * sizeBlock], &matrixC[i * sizeMatrix * sizeBlock + j * sizeBlock], sizeMatrix, sizeIBlock, sizeJBlock, sizeKBlock);
+      }
+    }
+  }
 }
 
-void mullBlocks(float *matrixA, float *matrixB, float *matrixC, int sizeBlock) {
-  for(int i = 0; i < sizeBlock; i++)
-    for(int k = 0; k < sizeBlock; k++)
-      for(int j = 0; j < sizeBlock; j++)
-        matrixC[i * sizeBlock + j] += matrixA[i * sizeBlock + k] * matrixB[k * sizeBlock + j];
+void mullBlocks(float *matrixA, float *matrixB, float *matrixC, int sizeMatrix, int sizeIBlock, int sizeJBlock, int sizeKBlock) {
+  for(int i = 0; i < sizeIBlock; i++)
+    for(int k = 0; k < sizeKBlock; k++)
+      for(int j = 0; j < sizeJBlock; j++)
+        *(matrixC + i * sizeMatrix + j) += *(matrixA + i * sizeMatrix + k) * *(matrixB + k * sizeMatrix + j);
 }
 
 void mullMatrix(float *matrixA, float *matrixB, float *matrixC, int sizeMatrix) {
   for(int i = 0; i < sizeMatrix; i++)
-    for(int j = 0; j < sizeMatrix; j++)
-      for(int k = 0; k < sizeMatrix; k++)
+    for(int k = 0; k < sizeMatrix; k++)
+      for(int j = 0; j < sizeMatrix; j++)
         matrixC[i * sizeMatrix + j] += matrixA[i * sizeMatrix + k] * matrixB[k * sizeMatrix + j];
 }
 
@@ -111,6 +162,13 @@ void fillMatrixRandElem(float *matrix, int sizeMatrix) {
   for(int i = 0; i < sizeMatrix; i++)
     for(int j = 0; j < sizeMatrix; j++)
       matrix[i * sizeMatrix + j] = rand() % MAX_RAND_DIV + 1;
+}
+
+void fillMatrixZeroElem(float *matrix, int sizeMatrix) {
+  srand(time(NULL));
+  for(int i = 0; i < sizeMatrix; i++)
+    for(int j = 0; j < sizeMatrix; j++)
+      matrix[i * sizeMatrix + j] = 0;
 }
 
 float* allocMemMatrix(int sizeMatrix) {
